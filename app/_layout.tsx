945ea56f7d8579f536629d1e64300f9357c6981d
Jason Korol <ejkorol@gmail.com>
2024-08-14 22:23:19 -0400

    integrate custom fonts into app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,11 @@
 import { Stack } from "expo-router";
+import { useCustomFonts } from "@/hooks/useCustomFonts";
 
 export default function RootLayout() {
+  const { fontsLoaded, error } = useCustomFonts();
+
+  if (!fontsLoaded && !error) return null;
+
   return (
     <Stack>
       <Stack.Screen name="index" />
--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,8 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex items-center justify-center h-full">
-      <Text className="text-center text-4xl">Edit app/index.tsx to edit this screen</Text>
+      <Text className="text-center font-inter text-4xl">{`Inter ->`}</Text>
+      <Text className="font-space">{`Space Mono ->`}</Text>
     </View>
   );
 }
