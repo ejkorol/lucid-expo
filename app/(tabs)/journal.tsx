1d7a411c4186af38b9b69107e5dbf33db331f751
Jason Korol <ejkorol@gmail.com>
2024-08-31 23:41:29 -0400

    add init journal view with static data

--- app/(tabs)/journal.tsx
+++ app/(tabs)/journal.tsx
@@ -1,10 +1,51 @@
-import { View, Text, SafeAreaView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView
+} from "react-native";
+
+const entries = [
+  {
+    id: 1,
+    title: 'Dream I had last night',
+    date: '09/1/2024',
+    content: 'It was crazy, I was a bird flying...'
+  },
+  {
+    id: 2,
+    title: 'Dreamt about my ex again...',
+    date: '08/31/2024',
+    content: 'Im so sick of seeing her everywhere'
+  }
+]
 
 const Journal = () => {
   return (
     <SafeAreaView>
-      <View>
-        <Text>Journal</Text>
+      <View className="px-6 py-8">
+        <View>
+          <Text className="font-inter text-4xl">Journal</Text>
+        </View>
+        <View className="mt-12">
+          {entries.map((entry) => (
+            <View
+              key={entry.id}
+              className="mb-6 border-b pb-6 border-solid border-[#bdbdbd]"
+            >
+              <View className="flex flex-row justify-between">
+                <Text className="font-inter text-xl">
+                  {entry.title}
+                </Text>
+                <Text className="font-space text-sm text-[#bdbdbd]">
+                  {entry.date}
+                </Text>
+              </View>
+              <Text className="font-inter text-sm text-[#757575]">
+                {entry.content}
+              </Text>
+            </View>
+          ))}
+        </View>
       </View>
     </SafeAreaView>
   );
