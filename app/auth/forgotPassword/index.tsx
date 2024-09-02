f986234436479267c15b206596f9987f77bc991d
Jason Korol <ejkorol@gmail.com>
2024-09-02 16:24:42 -0400

    add initial view for resetting password

---
+++ app/auth/forgotPassword/index.tsx
@@ -0,0 +1,17 @@
+import {
+  View,
+  Text,
+  SafeAreaView
+} from "react-native";
+
+const ForgotPassword = () => {
+  return (
+    <SafeAreaView>
+      <View className="px-6 py-8">
+        <Text>Forgot password?</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default ForgotPassword;
